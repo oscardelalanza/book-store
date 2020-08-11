0754c878043b11ab53c2fa8556ac8cf7edbfc58e
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-08-11 16:10:54 -0700

    bookslist table header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import BooksList from './BooksList';
 
 const App = () => (
   <div>
---
+++ src/components/BooksList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const BooksList = () => (
+  <table>
+    <tr>
+      <th>Book ID</th>
+      <th>Title</th>
+      <th>Category</th>
+    </tr>
+  </table>
+);
+
+export default BooksList;
