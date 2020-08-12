7a0eade05e7f58e41ce227cb64b1ed7b36e1f399
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-08-11 22:07:22 -0700

    book from with inputs

--- src/components/BooksForm.jsx
+++ src/components/BooksForm.jsx
@@ -1,8 +1,27 @@
 import React from 'react';
 
+const categories = ['Action', 'Biography', 'History', 'Horror', 'Kids', 'Learning', 'Sci-Fi'];
+
 const BooksForm = () => (
   <div>
-    <h4>the form</h4>
+    <h3>Add New Book</h3>
+    <form>
+      <div>
+        <label htmlFor="title">
+          Title:
+          <input type="text" id="title" placeholder="Book Title" />
+        </label>
+      </div>
+      <div>
+        <label htmlFor="category">
+          Category:
+          <select id="category">
+            <option selected>Select Category</option>
+            {categories.map(cat => <option key={cat} value={cat}>{cat}</option>)}
+          </select>
+        </label>
+      </div>
+    </form>
   </div>
 );
 
