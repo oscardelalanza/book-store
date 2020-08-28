05495ac0bc404d6946bb6068437158f6304cb68e
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-08-27 18:38:43 -0700

    removebook connected to booklist

--- src/containers/BooksList.jsx
+++ src/containers/BooksList.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Book from '../components/Book';
+import { removeBook } from '../actions';
 
 const BooksList = ({ books }) => (
   <table>
     <thead>
-      <tr>
-        <th>Book ID</th>
-        <th>Title</th>
-        <th>Category</th>
-      </tr>
+      <th>Book ID</th>
+      <th>Title</th>
+      <th>Category</th>
+      <th>Remove</th>
     </thead>
     <tbody>
       {books.map(book => <Book key={book.id} book={book} />)}
@@ -21,8 +21,12 @@ const mapStateToProps = state => ({
   books: state.books,
 });
 
+const mapDispatchToProps = dispatch => ({
+  removeBook: book => (dispatch(removeBook(book))),
+});
+
 BooksList.propTypes = {
   books: PropTypes.arrayOf(PropTypes.object).isRequired,
 };
 
-export default connect(mapStateToProps, {})(BooksList);
+export default connect(mapStateToProps, mapDispatchToProps)(BooksList);
