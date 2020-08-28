a7475d611fff8c3a0d565a0c891f6fb36c1f3bf9
Oscar De La Lanza <oscardelalanza@gmail.com>
2020-08-28 14:36:40 -0700

    reducer createbook optimized

--- src/reducers/books.js
+++ src/reducers/books.js
@@ -3,7 +3,7 @@ import { CREATE_BOOK, REMOVE_BOOK } from '../actions';
 const bookReducer = (state = {}, action) => {
   switch (action.type) {
     case CREATE_BOOK:
-      return [...state].concat(action.book);
+      return [...state, action.book];
 
     case REMOVE_BOOK:
       return [...state].filter(book => book.id !== action.book.id);
